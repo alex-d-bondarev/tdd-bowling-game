d527130a5fd10887607d61d38542ddef5d371cd4
Alex <alex.d.bondarev@gmail.com>
2020-03-07 13:26:44 +0200

    add first step

---
+++ src/test/java/org/practice/app/step1/BowlingTest.java
@@ -0,0 +1,10 @@
+package org.practice.app.step1;
+
+import org.junit.Test;
+
+public class BowlingTest {
+    @Test
+    public void nothing(){
+        System.out.println("Confirm project is configured correctly.");
+    }
+}

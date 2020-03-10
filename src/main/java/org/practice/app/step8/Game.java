a4df37787ab0057c697f55eaf2e67c43d01ea381
Alex <alex.d.bondarev@gmail.com>
2020-03-10 21:58:50 +0200

    add step 8

---
+++ src/main/java/org/practice/app/step8/Game.java
@@ -0,0 +1,48 @@
+package org.practice.app.step8;
+
+public class Game {
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int firstInRound = 0;
+        for (int round = 0; round < 10; round++) {
+            if (isStrike(rolls[firstInRound])) {
+                score = 10 + nextTwoBallsForStrike(firstInRound);
+                firstInRound++;
+            } else if (isSpare(firstInRound)) {
+                score += 10 + nextBallForSpare(firstInRound);
+                firstInRound += 2;
+            } else {
+                score += twoBallsInRound(firstInRound);
+                firstInRound += 2;
+            }
+        }
+        return score;
+    }
+
+    private int twoBallsInRound(int firstInRound) {
+        return rolls[firstInRound] + rolls[firstInRound + 1];
+    }
+
+    private int nextBallForSpare(int firstInRound) {
+        return rolls[firstInRound + 2];
+    }
+
+    private int nextTwoBallsForStrike(int firstInRound) {
+        return rolls[firstInRound + 1] + rolls[firstInRound + 2];
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
+    private boolean isSpare(int firstInRound) {
+        return rolls[firstInRound] + rolls[firstInRound + 1] == 10;
+    }
+}
---
+++ src/test/java/org/practice/app/step8/BowlingTest.java
@@ -0,0 +1,64 @@
+package org.practice.app.step8;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.practice.app.step8.Game;
+
+import static org.junit.Assert.assertEquals;
+
+public class BowlingTest {
+    private Game g;
+
+    @Before
+    public void setUp(){
+        g = new Game();
+    }
+
+    @Test
+    public void gutterGame(){
+        rollGameOf(0);
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void allOnes(){
+        rollGameOf(1);
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void oneSPare(){
+        rollSpare();
+        g.roll(1);
+        rollMany(17, 0);
+        assertEquals(12, g.score());
+    }
+
+    @Test
+    public void oneStrike(){
+        rollStrike();
+        g.roll(1);
+        g.roll(1);
+        rollMany(16, 0);
+        assertEquals(14, g.score());
+    }
+
+    private void rollStrike() {
+        g.roll(10);
+    }
+
+    private void rollGameOf(int pins) {
+        rollMany(20, pins);
+    }
+
+    private void rollMany(int times, int pins){
+        for (int i = 0; i < times; i++) {
+            g.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+}
